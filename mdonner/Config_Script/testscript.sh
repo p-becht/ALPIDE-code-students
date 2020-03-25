b324ada179fc6fab6be3304e7e068f3323a23946
Marmorexx <maurice@mdonner.com>
2020-03-26 00:03:39 +0100

    Added python script to write in cfg

---
+++ mdonner/Config_Script/testscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VALUE1=$(cat Config.txt | grep 'Value1')
+VALUE2=$(cat Config.txt | grep 'VCASN')
+VALUE3=$(cat Config.txt | grep 'VCASN2')
+VALUE4=$(cat Config.txt | grep 'ITHR')
+
+echo "Running Threshold test with the following values:"
+echo "$VALUE1"
+echo "$VALUE2"
+echo "$VALUE3"
+echo "$VALUE4"
