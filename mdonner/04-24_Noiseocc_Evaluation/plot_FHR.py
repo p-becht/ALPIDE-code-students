2516f9d11691b538741d65f191ab1bd1acc038bd
Maurice Donner <maurice@X220>
2020-04-28 14:27:55 +0200

    28-04-20

--- mdonner/04-24_Noiseocc_Evaluation/plot_FHR.py
+++ mdonner/04-24_Noiseocc_Evaluation/plot_FHR.py
@@ -1,7 +1,14 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3.5
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+csv_Threshold_0 = "../04-22_Threshold_Evaluation/bb0_output.csv"
+csv_Noise_0 = "0bb_output.csv"
+csv_Threshold_3 = "../04-22_Threshold_Evaluation/bb3_output.csv"
+csv_Noise_3 = "3bb_output.csv"
+
+# {{{ Functions
 
 def getValues(array):
     #Create a temporary list
@@ -19,34 +26,102 @@ def getValues(array):
 
 #Function to extract Values in the right order
 def ExtractValues(VCASN_in,ITHR_in,array):
-    output = np.ndarray((len(VCASN_in),len(ITHR_in)))
+    output = np.ndarray((len(VCASN_in)*len(ITHR_in)))
     for i in range(len(VCASN_in)):
         for j in range(len(ITHR_in)):
-            output[i,j] = array[(VCASN == VCASN_in[i]) & (ITHR == ITHR_in[j])]
+            output[j+len(ITHR_in)*i] = array[(VCASN == VCASN_in[i]) & (ITHR == ITHR_in[j])]
     return output
 
-#Load 0bb Data
-VCASN, ITHR, Threshold = np.loadtxt("../04-22_Threshold_Evaluation/bb0_output.csv", skiprows=1, usecols=(1,2,3), delimiter=",", unpack=True)
+#Function to check for mismatched Elements between Arrays
+def mismatch(VCASN_1, VCASN_2, ITHR_1, ITHR_2):
+    for i in range(len(VCASN_1)):
+        if (VCASN_1[i] - VCASN_2[i]) == 0: continue
+        print("Error, mismatched element in VCASN detected")
+    for i in range(len(ITHR_1)):
+        if (ITHR_1[i] - ITHR_2[i]) == 0: continue
+        print("Error, mismatched element in ITHR detected")
+
+# }}}
+
+# {{{ Data
+
+#Load 0bb Threshold Data
+VCASN, ITHR, Threshold = np.loadtxt(csv_Threshold_0, skiprows=1, usecols=(1,2,3), delimiter=",", unpack=True)
 VCASN_0 = getValues(VCASN)
 ITHR_0 = getValues(ITHR)
-TRSH = ExtractValues(VCASN_0,ITHR_0,Threshold)
+TRSH_0 = ExtractValues(VCASN_0,ITHR_0,Threshold)
 
-# NoiseOcc data with 100000 Triggers
-VCASN, ITHR, Noise, Noise_Err = np.loadtxt("0bb_output.csv", skiprows=1, usecols=(1,2,4,5), delimiter=",", unpack=True)
+#Load 0bb Noiseocc Data
+VCASN, ITHR, Ntriggers, Noise, Noise_Err = np.loadtxt(csv_Noise_0, skiprows=1, usecols=(1,2,3,4,5), delimiter=",", unpack=True)
 VCASN_0_Noise = getValues(VCASN)
 ITHR_0_Noise = getValues(ITHR)
-NOISE = ExtractValues(VCASN_0_Noise,ITHR_0_Noise,Noise)
-NOISE_ERR = ExtractValues(VCASN_0_Noise,ITHR_0_Noise,Noise_Err)
+NOISE_0 = ExtractValues(VCASN_0_Noise,ITHR_0_Noise,Noise)
+NOISE_0_ERR = ExtractValues(VCASN_0_Noise,ITHR_0_Noise,Noise_Err)
 
-#Check for mismatched Elements
-for i in range(len(VCASN_0)):
-    if (VCASN_0[i] - VCASN_0_Noise[i]) == 0: continue
-    print("Error, mismatched element in VCASN detected")
-for i in range(len(ITHR_0)):
-    if (ITHR_0[i] - ITHR_0_Noise[i]) == 0: continue
-    print("Error, mismatched element in ITHR detected")
+#Check for mismatched values
+mismatch(VCASN_0, VCASN_0_Noise, ITHR_0, ITHR_0_Noise)
 
-#TODO Extract one value each from threshold and Noise and fit them NOICELY together
-xaxis = []
-yaxis = []
-for i in range(len
+#Load 3bb Threshold Data
+VCASN, ITHR, Threshold = np.loadtxt(csv_Threshold_3, skiprows=1, usecols=(1,2,3), delimiter=",", unpack=True)
+VCASN_3 = getValues(VCASN)
+ITHR_3 = getValues(ITHR)
+TRSH_3 = ExtractValues(VCASN_3,ITHR_3,Threshold)
+
+#Load 3bb NoiseOcc Data
+VCASN, ITHR, Ntriggers, Noise, Noise_Err = np.loadtxt(csv_Noise_3, skiprows=1, usecols=(1,2,3,4,5), delimiter=",", unpack=True)
+VCASN_3_Noise = getValues(VCASN)
+ITHR_3_Noise = getValues(ITHR)
+NOISE_3 = ExtractValues(VCASN_3_Noise,ITHR_3_Noise,Noise)
+NOISE_3_ERR = ExtractValues(VCASN_3_Noise,ITHR_3_Noise,Noise_Err)
+
+#Check for mismatched values
+mismatch(VCASN_0, VCASN_0_Noise, ITHR_0, ITHR_0_Noise)
+
+# }}}
+
+# {{{ Plot 
+
+# Plot section #################################################################
+senlim = min(NOISE_3)
+x_val_0 = [x for x in TRSH_0]
+y_val_0 = [y for y in NOISE_0]
+y_err_0 = [y for y in NOISE_0_ERR]
+x_val_3 = [x for x in TRSH_3]
+y_val_3 = [y for y in NOISE_3]
+y_err_3 = [y for y in NOISE_3_ERR]
+
+plt.figure(figsize=(12,10))
+
+#Plot 0bb 
+plt.subplot(211)
+
+plt.errorbar(x_val_0, y_val_0, xerr=None, yerr=y_err_0, fmt='.',capsize=3, c='red')
+
+plt.yscale("log")
+plt.xlim(1.5,26.5)
+plt.ylim(1e-11,1e-4)
+plt.hlines(senlim, min(x_val_0), max(x_val_0), linewidth=1, linestyle="--")
+plt.text(22.5, 2*senlim, "Sensitivity Limit", fontsize=12)
+
+plt.xlabel("Threshold in DAC Values")
+plt.ylabel("Fake hit rate")
+plt.title("V_BB = 0 V")
+
+#Plot 3bb 
+plt.subplot(212)
+
+plt.errorbar(x_val_3, y_val_3, xerr=None, yerr=y_err_3, fmt='.' ,capsize=3, c='red')
+
+plt.yscale("log")
+plt.xlim(1.5,26.5)
+plt.ylim(1e-11,1e-4)
+plt.hlines(senlim, min(x_val_0), max(x_val_0), linewidth=1, linestyle="--")
+plt.text(22.5, 2*senlim, "Sensitivity Limit", fontsize=12)
+
+plt.xlabel("Threshold in DAC Values")
+plt.ylabel("Fake hit rate")
+plt.title("V_BB = 0 V")
+plt.subplots_adjust(wspace=2)
+plt.savefig("Fake_Hit_Rate.pdf")
+
+# }}}
