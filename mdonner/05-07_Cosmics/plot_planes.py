bf5876dd38446389f0e939492131ca9004821844
Maurice Donner <maurice@X220>
2020-05-18 13:48:51 +0200

    18-05-20

--- mdonner/05-07_Cosmics/plot_planes.py
+++ mdonner/05-07_Cosmics/plot_planes.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#!/usr/bin/python3.5
 
 from itertools import takewhile
 import re 
@@ -10,14 +10,14 @@ from pprint import pprint
 txt_file = sys.argv[1] 
 
 def eventData(data,event):
-    marker = f"=== {event} ==="
+    marker = "=== "+str(event)+" ==="
     if marker not in data: return []  
     start = data.index(marker)+1
     print (data[start-1])
     return list(takewhile(lambda d:not d.startswith("==="),data[start:]))  
-
+    
 def planeData(data,plane):
-    marker = f"--- pALPIDEfs_{plane} ---"
+    marker = "--- pALPIDEfs_"+str(plane)+" ---"
     if marker not in data: return []
     start = data.index(marker)+1
     return list(takewhile(lambda d:not d.replace("=","-").startswith("---"),data[start:]))
@@ -31,10 +31,10 @@ def selectXandY(line):
 
 with open(txt_file) as file:
     data = file.read().split('\n')
-    event_number = str(re.findall(r'\b\d+\b', data[int(sys.argv[2])-4])[0])   # adapted for a quick checkup; cat 'file' | grep -n 'pALPIDE_' ; this outputs txt line number; go back 4 lines once an event line is identified to get event number
+    event_number = str(re.findall(r'\b\d+\b', data[int(sys.argv[2])-2])[0])   # adapted for a quick checkup; cat 'file' | grep -n 'pALPIDE_' ; this outputs txt line number; go back 4 lines once an event line is identified to get event number
     event = eventData(data,event_number)
     pprint (event)
-    print ()
+    print(data[int(sys.argv[2])-4])
     for i in range(0,7):
         print(i)
         im=np.zeros((512,1024))
@@ -43,7 +43,7 @@ with open(txt_file) as file:
             for pair in selectXandY(line):
                 im[int(pair[1]),int(pair[0])] =1
         plt.figure(i)
-        plt.xlim([500,775])
-        plt.ylim([150,375])
+        plt.xlim([0,1023])
+        plt.ylim([0,511])
         plt.imshow(im)
         plt.show()
