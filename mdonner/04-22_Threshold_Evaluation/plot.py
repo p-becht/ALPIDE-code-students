aeb2a6f03347b33f956fc89cc320ed3f050276ae
Maurice Donner <maurice@X220>
2020-05-28 11:56:46 +0200

    28-05-20

--- mdonner/04-22_Threshold_Evaluation/plot.py
+++ mdonner/04-22_Threshold_Evaluation/plot.py
@@ -5,7 +5,7 @@ import sys
 
 csv = sys.argv[1]
 #Import data
-VCASN, ITHR, TRSH = np.loadtxt(csv, skiprows=1, usecols=(1,2,3), delimiter=",", unpack=True)
+VCASN, ITHR, TRSH, TRSH_Err = np.loadtxt(csv, skiprows=1, usecols=(1,2,3,4), delimiter=",", unpack=True)
 
 #These arrays list entries multiple times. What will be beneficial for this
 #Kind of plot is, to have ITHR and VCASN as X and Y axis respectively,
@@ -37,25 +37,38 @@ for i in range(len(VCASN_0)):
         Threshold[i,j] = TRSH[(VCASN == VCASN_0[i]) & (ITHR == ITHR_0[j])]
 #######################################
 
-for png in ["Heatmap.png","Heatmap_soft.png"]:
+#####  Do the same for the Error  #####
+Threshold_Error = np.ndarray((len(VCASN_0),len(ITHR_0)))
+for i in range(len(VCASN_0)):
+    for j in range(len(ITHR_0)):
+        Threshold_Error[i,j] = TRSH_Err[(VCASN == VCASN_0[i]) & (ITHR == ITHR_0[j])]
+#######################################
     
-    #Plotting
-    fig, ax = plt.subplots(figsize=(len(ITHR_0),len(VCASN_0)))
-    im = ax.imshow(Threshold)
+#Plotting
+fig, ax = plt.subplots(figsize=(len(ITHR_0),len(VCASN_0)))
+im = ax.imshow(Threshold)
 
-    ##Show all ticks and label them correctly
-    ax.set_xticks(np.arange(len(ITHR_0)))
-    ax.set_yticks(np.arange(len(VCASN_0)))
-    ax.set_xticklabels(ITHR_0)
-    ax.set_yticklabels(VCASN_0)
-    ax.set_xlabel("ITRH")
-    ax.set_ylabel("VCASN")
+##Show all ticks and label them correctly
+ax.set_xticks(np.arange(len(ITHR_0)))
+ax.set_yticks(np.arange(len(VCASN_0)))
+ax.set_xticklabels(ITHR_0)
+ax.set_yticklabels(VCASN_0)
+ax.set_xlabel("ITRH")
+ax.set_ylabel("VCASN")
 
-    for i in range(len(VCASN_0)):
-        for j in range(len(ITHR_0)):
-            text = ax.text(j,i,int(10*Threshold[i,j]),ha="center", va="center", color="w")
-    ax.set_title("Threshold in Electrons")
+print("Include Errors? [y,n]")
+print_errors = str(input())
 
-    plt.savefig(png)
+for i in range(len(VCASN_0)):
+    for j in range(len(ITHR_0)):
+        #What to write into each square
+        if (print_errors == "y"):
+            square = str(int(10*Threshold[i,j]))+"+-"+str(round(10*Threshold_Error[i,j],1))
+        else:
+            square = int(10*Threshold[i,j])
+        text = ax.text(j,i,square,ha="center", va="center", color="w")
+ax.set_title("Threshold in Electrons")
 
-    plt.style.use('classic')
+plt.savefig("Heatmap.png")
+
+plt.style.use('classic')
